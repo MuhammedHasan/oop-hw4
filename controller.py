c57b9415bff3ed4a28e4c924e1eb8cdde52ef89e
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-10-25 14:36:06 +0300

    views

--- controller.py
+++ controller.py
@@ -4,10 +4,10 @@ from model import Model
 class Controller:
 
     def get(self):
-        return open('entry.html').read()
+        return open('views/entry.html').read()
 
     def post(self, data):
         m = Model(data['name'], data['surname'])
         if m.validate():
-            return 'Ok'
-        return 'Not Ok'
+            return open('views/ok.html').read()
+        return open('views/notok.html').read()
